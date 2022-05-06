d0424362d86cae398c5b324aecdf1dd74b8904a3
fegritr <fegrit.r@gmail.com>
2022-05-06 17:41:14 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <title>{{env('APP_NAME')}}</title>
+    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
+    <link href="{{ mix('css/template.css') }}" type="text/css" rel="stylesheet"/>
+    <!--begin::Fonts-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+</head>
+<body>
+@if (Auth::check())
+    <script>
+        window.Laravel = {!!json_encode([
+            'isLoggedin' => true,
+            'user' => Auth::user()
+        ])!!}
+    </script>
+@else
+    <script>
+        window.Laravel = {!!json_encode([
+            'isLoggedin' => false
+        ])!!}
+    </script>
+@endif
+<div id="app">
+</div>
+<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+<script src="{{ mix('js/template.js') }}" type="text/javascript"></script>
+</body>
+</html>
